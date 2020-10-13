cdec406f602a6251c3f5855596bc7cf7ff19c089
Christian Bodenwinkler <c.bodenwinkler@gmx.at>
2020-10-13 14:52:05 +0200

    Basic2

---
+++ basic/basic2/basic2.js
@@ -0,0 +1,24 @@
+// 1. Print your full name in the browser.
+var obj = {
+    name: 'Christian',
+    lastName: 'Bodenwinkler',
+    age: 29
+};
+// console.table(obj);
+// const{name: n, lastName: l} = obj;
+// document.write(`<h1>${n} ${l}</h1>`);
+//2. Print your first name 10 times in the browser using a forEach loop.
+function sayLater() {
+    console.log(obj.name);
+    setTimeout(function () {
+        console.log(obj.lastName);
+    }, 4000);
+}
+sayLater();
+//3. A function should be triggered 5 seconds after opening the page that prints your last name in the console 10 times.
+// setTimeout(() => {
+//     let bw = `${obj.lastName}\n`;
+//     console.log(bw.repeat(10));
+//     let bw2 = `${obj.lastName} <br>`;
+//     document.write(bw2.repeat(10));
+// },5000);
